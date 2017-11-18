ad7c357311bad79e4321ec0e94879f698377136c
Hiroyuki Inoue <hiroyukiinoue@Hiroyukis-MacBook-Pro.local>
2017-11-18 03:05:41 +0200

    first upload

--- linked_list/circular_linked_list.py
+++ linked_list/circular_linked_list.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Wed Oct 11 01:50:13 2017
+
+@author: hiroyukiinoue
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Tue Oct 10 22:42:08 2017
 
 @author: hiroyukiinoue
@@ -29,7 +37,7 @@ class Node():
         return self.nextt
 
 
-class SingleLinkedList():
+class CircularLinkedList():
     '''creat a doubly linked list'''
     def __init__(self):
         self.head = None
@@ -47,19 +55,6 @@ class SingleLinkedList():
                 print('curr = %d, next = %d' % (temp.value, temp.get_next().value))
                 temp = temp.get_next()
             print('curr = %d' % (temp.value))
-
-    def str_print_list(self):
-        '''print the content of the list'''
-        if self.len == 0:
-            return '|'
-        else:
-            temp = self.head
-            strr = ''
-            while temp.get_next() != None:
-                strr += str(temp.value) + ' ,'
-                temp = temp.get_next()
-            strr += str(temp.value) + '|'
-            return strr[::-1]
 
     def find_node(self, node):
         '''delete the head node of the list'''
@@ -205,126 +200,57 @@ class SingleLinkedList():
                 nxt = None
                 self.len -= 1
 
-    def delete_nth_last_node(self, n):
+    def loop_to_nth_last_node(self, n):
         if n > self.len:
             raise Exception('The length of this list is less than the specified number')
-        elif n <= 0:
-            raise Exception('You have to pass an positive integer.')
-        elif n == 1:
-            self.delete_tail_node()
+        elif n <= 1:
+            raise Exception('You have to pass an integer bigger than 1.')
         else:
             temp = self.head
             nxt = temp
-            for i in range(1,n):
+            for _ in range(1,n):
                 nxt = nxt.get_next()
             while nxt.get_next() != None:
                 temp = temp.get_next()
                 nxt = nxt.get_next()
-            mid = temp
-            nxt = temp.get_next()
-            if nxt is None:
-                mid = None
-                self.len -= 1
-            else:
-                mid.value = nxt.value
-                mid.set_next(nxt.get_next())
-                nxt = None
-                self.len -= 1
-
-    def pop_head_node(self):
-        '''delete the head node of the list'''
+            nxt.set_next(temp)
+    
+    def find_loop_head(self):
+        '''delete the specified node from the middle of the list'''
         if self.len == 0:
-            return None
-        else:
-            temp = self.head
-            self.head = temp.get_next()
-            self.len -= 1
-            return temp
+            raise Exception('This list is empty.')
+        elif self.len == 1:
+            raise Exception('Only one element in this list.')
+        run1 = self.head.get_next()
+        run2 = self.head.get_next().get_next()
+        while run1.value != run2.value:
+            run1 = run1.get_next()
+            run2 = run2.get_next().get_next()
+            if run2 is None:
+                print('No loop in this list.')
+                break
+        if run2 != None:
+            run1 = self.head
+            while run1.value != run2.value:
+                run1 = run1.get_next()
+                run2 = run2.get_next()
+                return run1
 
-    def erase_dups(self):
-        '''delete the head node of the list'''
-        temp = self.head
-        run = self.head
-        while temp.get_next() != None:
-            while run.get_next() != None:
-                temp = temp.get_next()
 
-    def erase_dups_hash(self):
-        if self.len == 0:
-            return True
-        if self.len == 1:
-            return True
-        from hash_table import HashTable
-        hsh = HashTable(23)
-        temp = self.head
-        hsh.put(temp.value, 1)
-        nxt = temp.get_next()
-        while nxt.get_next() != None:
-            cnt = hsh.get(nxt.value)
-            if cnt is None:
-                hsh.put(nxt.value, 1)
-            else:
-                cnt += 1
-                hsh.put(temp.value, cnt)
-                temp.value = temp.get_next().value
-                temp.set_next(nxt.get_next())
-            temp = nxt
-            nxt = nxt.get_next()
-
-def convert_to_list(n):
-    if type(n) != int:
-        raise Exception('You must pass an integer')
-    else:
-        ill = SingleLinkedList()
-        while n//10 != 0:
-            r = n % 10
-            n = n // 10
-            ill.insert_at_tail(Node(r))
-        ill.insert_at_tail(Node(n))
-        return ill
-
-def add_two_lists(ls1, ls2):
-    ls = SingleLinkedList()
-    carry = 0
-    while not(ls1.len == 0 and ls2.len == 0):
-        if ls1.len == 0:
-            d1 = 0
-        else:
-            d1 = ls1.pop_head_node().value
-        if ls2.len == 0:
-            d2 = 0
-        else:
-            d2 = ls2.pop_head_node().value
-        ad = d1 + d2
-        r = ad % 10
-        ls.insert_at_tail(Node(r+carry))
-        carry = ad // 10
-    if carry == 1:
-        ls.insert_at_tail(Node(carry))
-    return ls
-            
 def __main():
     print('')
-    kaka = SingleLinkedList()
-    for i in range(10):
+    kaka = CircularLinkedList()
+    for i in range(50):
         kaka.insert_at_tail(Node(i))
     kaka.show_the_list()
     print('')
-    strr = kaka.str_print_list()
-    print(strr)
     
-    #ls = add_two_lists(convert_to_list(1234), convert_to_list(456))
-    #ls.show_the_list()
-    #print('')
-    
-    kaka.insert_in_middle(Node(5), Node(5))
-    kaka.show_the_list()
-    print('')
-    kaka.erase_dups_hash()
-    kaka.show_the_list()
-    print('')
+    kaka.loop_to_nth_last_node(11)
+    lph = kaka.find_loop_head()
+    print(lph.value)
 
 if __name__ == "__main__":
     __main()
 else:
-    print('singly_linked_list.py is imported')
+    print('circular_linked_list.py is imported')
+            
--- linked_list/doubly_linked_list.py
+++ linked_list/doubly_linked_list.py
@@ -1,188 +1,200 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep  9 22:57:27 2017
+Created on Tue Oct 10 18:29:04 2017
 
 @author: hiroyukiinoue
 """
 
-class Node:
-    """create a node"""
-    def __init__(self, data, pprev=None, nnext=None):
-        self.data = data
-        self.prev = pprev
-        self.next = nnext
+class Node():
+    '''creat a node'''
+    def __init__(self, value):
+        self.value = value
+        self.prevv = None
+        self.nextt = None
 
-    def get_data(self):
-        """get the value of this node"""
-        return self.data
+    def set_value(self, value):
+        '''set the value of the node'''
+        self.value = value
 
-    def set_data(self, data):
-        """set the value of this node"""
-        self.data = data
-
-    def get_prev(self):
-        """get the previous node"""
-        return self.prev
-
-    def set_prev(self, node):
-        """set the previous node"""
-        self.prev = node
-
-    def get_next(self):
-        """get the next node"""
-        return self.next
+    def get_value(self):
+        '''get the value of the node'''
+        return self.value
 
     def set_next(self, node):
-        """set the next node"""
-        self.next = node
+        '''set the next node'''
+        self.nextt = node
+
+    def get_next(self):
+        '''get the next node'''
+        return self.nextt
+
+    def set_prev(self, node):
+        '''set the previous node'''
+        self.prevv = node
+
+    def get_prev(self):
+        '''get the previous node'''
+        return self.prevv
 
 
-class DoublyLinkedList:
-    """create a doubly linked list"""
+class DoubleLinkedList():
+    '''creat a doubly linked list'''
     def __init__(self):
         self.head = None
-        self.count = 0
+        self.len = 0
 
-    def print_list(self):
-        """prints the content of this doubly linked list"""
-        if self.head is None:
-            print('It''s empty!')
-        elif self.count == 1:
-            print('prev =  , current = %d, next =  ' % (self.head.data))
-        else:
-            current = self.head
-            while current.get_next() != None:
-                if current.get_prev() != None:
-                    print('prev = %d, current = %d, next = %d'\
-        % (current.get_prev().data, current.data, current.get_next().data))
-                else:
-                    print('prev =  , current = %d, next = %d' \
-        % (current.data, current.get_next().data))
-                current = current.get_next()
-
-    def insert_at_head(self, data):
-        """inserts a new node with value data at the head of the list"""
-        if self.head is None:
-            self.head = Node(data)
-            self.count += 1
-        else:
-            current = Node(data)
-            current.set_next(self.head)
-            self.head.set_next(current)
-            self.head = current
-            self.count += 1
-
-    def insert_at_tail(self, data):
-        """inserts a new node with value data at the end of the list"""
-        if self.head is None:
-            self.head = Node(data)
-            self.count += 1
-        else:
-            current = self.head
-            while current.get_next() != None:
-                current = current.get_next()
-            current.set_next(Node(data, current))
-            self.count += 1
-
-    def insert_in_middle(self, middle, data):
-        """inserts a new node with value data in the middle of the list
-        right after a node with value "middle"
-        """
-        if self.head is None and middle != 0:
-            print('The list is empty!')
-            return None
-        elif self.head is None and middle == 0:
-            self.head = Node(data)
-            self.count += 1
-        else:
-            middle = self.get_node(middle)
-            middle_next = middle.get_next()
-            temp = Node(data, middle, middle_next)
-            middle.set_next(temp)
-            middle_next.set_prev(temp)
-
-    def get_node(self, node):
-        """return a node with value "node" """
-        if self.head is None:
-            print('The list is empty!')
-            return None
-
-        current = self.head
-        while current.get_next().data != node:
-            current = current.get_next()
-        
-        if current == None:
-            raise Exception('Such node does not exist in this list!')
-        else:
-            return current.get_next()
-
-    def delete_head(self):
-        """delete the node at the head"""
-        if self.head is None:
-            raise Exception('The list is empty!')
-        elif self.count == 1:
-            self.head = None
-            self.count -= 1
+    def show_the_list(self):
+        '''print the content of the list'''
+        if self.len == 0:
+            print('This list is empty')
+        elif self.len == 1:
+            print('value = %d' % (self.head.value))
         else:
             temp = self.head
-            self.head = temp.get_next()
-            temp.get_next().set_prev(None)
+            while temp.get_next() != None:
+                if temp == self.head:
+                    print('          curr = %d, next = %d' \
+                                          %(temp.value, temp.get_next().value))
+                    temp = temp.get_next()
+                else:
+                    print('prev = %d, curr = %d, next = %d' \
+                   %(temp.get_prev().value, temp.value, temp.get_next().value))
+                    temp = temp.get_next()
+            print('prev = %d, curr = %d' % (temp.get_prev().value, temp.value))
+
+    def find_node(self, node):
+        '''delete the head node of the list'''
+        if self.len == 0:
+            raise Exception('This list is empty')
+        else:
+            temp = self.head
+            while temp.value != node.value:
+                temp = temp.get_next()
+                if temp is None:
+                    return False
+            return True
+
+    def insert_at_head(self, node):
+        '''insert a new node at the head of the list'''
+        if self.len == 0:
+            self.head = node
+            self.len += 1
+        else:
+            temp = self.head
+            temp.set_prev(node)
+            node.set_next(temp)
+            self.head = node
+            self.len += 1
+
+    def insert_at_tail(self, node):
+        '''insert a new node at the head of the list'''
+        if self.len == 0:
+            self.head = node
+            self.len += 1
+        else:
+            temp = self.head
+            while temp.get_next() != None:
+                temp = temp.get_next()
+            temp.set_next(node)
+            node.set_prev(temp)
+            self.len += 1
+
+    def insert_in_middle(self, middle, node):
+        '''insert a new node right after "middle" node'''
+        if self.len == 0:
+            raise Exception('This list is empty and the specified node does not exist.')
+        elif self.len == 1:
+            if middle.value == self.head.value:
+                middle.set_next(node)
+                node.set_prev(middle)
+                self.len += 1
+            else:
+                raise Exception('The specified node does not exist.')
+        else:
+            temp = self.head
+            while temp.get_next().value != middle.value:
+                temp = temp.get_next()
+                if temp is None:
+                    raise Exception('The specified node does not exist in this list.')
+            temp = temp.get_next()
+            if temp.get_next() is None:
+                temp.set_next(node)
+                node.set_prev(temp)
+                self.len += 1
+            else:
+                nxt = temp.get_next()
+                temp.set_next(node)
+                node.set_prev(temp)
+                nxt.set_prev(node)
+                node.set_next(nxt)
+                self.len += 1
+
+    def delete_head_node(self):
+        '''delete the head node of the list'''
+        if self.len == 0:
+            raise Exception('This list is empty')
+        else:
+            temp = self.head
+            nxt = temp.get_next()
+            self.head = nxt
             temp = None
-            self.count -= 1
+            self.len -= 1
 
-    def delete_tail(self):
-        """delete the node at the tail"""
-        if self.head is None:
-            raise Exception('The list is empty!')
-        elif self.count == 1:
+    def delete_tail_node(self):
+        '''delete the tail node of the list'''
+        if self.len == 0:
+            raise Exception('This list is empty')
+        elif self.len == 1:
             self.head = None
-            self.count -= 1
+            self.len -= 1
         else:
-            current = self.head
-            while current.get_next() != None:
-                current = current.get_next()
-            current.get_prev().set_next(None)
-            current = None
-            self.count -= 1
+            temp = self.head
+            nxt = temp.get_next()
+            while nxt.get_next() != None:
+                temp = nxt
+                nxt = nxt.get_next()
+            temp.set_next(None)
+            nxt = None
+            self.len -= 1
 
-    def delete_middle(self, middle):
-        """delete the node with data "middle" in the middle of the list"""
-        if self.head is None:
-            raise Exception('The list is empty!')
-        elif self.count == 1:
-            self.head = None
-            self.count -= 1
+    def delete_middle_node(self, middle):
+        '''delete the specified node from the middle of the list'''
+        if self.len == 0:
+            raise Exception('This list is empty')
+        elif self.len == 1:
+            if middle.value == self.head.value:
+                self.head = None
+                self.len -= 1
+            else:
+                raise Exception('The specified node does not exist.')
         else:
-            current = self.head
-            while current.get_next().data != middle:
-                current = current.get_next()
-            current = current.get_next()
-            current_prev = current.get_prev()
-            current_next = current.get_next()
-            current_prev.set_next(current_next)
-            current_next.set_prev(current_prev)
-            current = None
-            self.count -= 1
+            temp = self.head
+            while temp.get_next().value != middle.value:
+                temp = temp.get_next()
+                if temp is None:
+                    raise Exception('The specified node does not exist in this list.')
+            mid = temp.get_next()
+            nxt = mid.get_next()
+            if nxt is None:
+                temp.set_next(None)
+                mid = None
+                self.len -= 1
+            else:
+                temp.set_next(nxt)
+                nxt.set_prev(temp)
+                mid = None
+                self.len -= 1
 
+def __main():
+    print('')
+    kaka = DoubleLinkedList()
+    for i in range(20):
+        kaka.insert_at_tail(Node(i))
+    kaka.show_the_list()
+    print('')
 
-DLL = DoublyLinkedList()
-DLL.insert_at_head(1)
-DLL.insert_at_tail(2)
-DLL.insert_at_tail(3)
-DLL.insert_at_tail(4)
-DLL.insert_at_tail(5)
-DLL.insert_at_tail(6)
-DLL.insert_at_tail(7)
-DLL.insert_at_tail(8)
-DLL.insert_in_middle(5, 15)
-DLL.print_list()
-print('')
-DLL.delete_head()
-DLL.print_list()
-print('')
-DLL.delete_tail()
-DLL.print_list()
-print('')
-DLL.delete_middle(15)
-DLL.print_list()
+if __name__ == "__main__":
+    __main()
+else:
+    print('doubly_linked_list.py is imported')
